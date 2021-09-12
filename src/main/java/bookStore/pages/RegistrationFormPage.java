83058d7300b971fc80025c22e022c4091753b610
Богданов Дмитрий Александрович <DBogdanov2@alfabank.ru>
2021-09-12 15:18:07 +0300

    init2

--- src/main/java/bookStore/pages/RegistrationFormPage.java
+++ src/main/java/bookStore/pages/RegistrationFormPage.java
@@ -2,14 +2,11 @@ package bookStore.pages;
 
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import ru.alfabank.alfatest.cucumber.api.AkitaPage;
 
 import java.util.List;
 
-public class RegistrationFormPage extends AkitaPage {
+public class RegistrationFormPage {
 
     @FindBy(id = "firstName")
     private SelenideElement firstName;
